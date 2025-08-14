cfa5f36060df2ee3ccef37b5b2a80085f73e20bb
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-08-14 15:59:30 -0400

    routerLinks desde componentes hijos.

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './gifs-side-menu-options.html',
 })
 export class GifsSideMenuOptions {
-
+  menuOptions = signal<MenuOption[]>([
+    {
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+      icon: 'fa-solid fa-chart-line',
+    },
+    {
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search',
+      icon: 'fa-solid fa-magnifying-glass',
+    },
+  ]);
 }
